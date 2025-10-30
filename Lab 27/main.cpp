409360957f0fa391544842481961f196d678977a
Nafi-GOAT <nafih2989@gmail.com>
2025-10-30 16:24:08 -0700

    Milestone 2

--- Lab 27/main.cpp
+++ Lab 27/main.cpp
@@ -14,10 +14,17 @@ int main() {
     // declarations
     map<string, tuple<int, string, string>> villagers;
 
-    // insert elements into the map
-    villagers["Audie"] = make_tuple(7, "Orange", "Yellow", "Red");
-    villagers["Raymond"] = {"Black", "Gray", "White"};
-    villagers.insert({"Marshal", {"Blue", "White", "Black"}});
+    villagers["Audie"] = make_tuple(7, "Wolf", "Foxtrot!");
+    villagers["Raymond"] = make_tuple(10, "Cat", "Nice fit!");
+    villagers.insert({"Marshal", make_tuple(9, "Squirrel", "Sulky...")});
+    
+    cout << "Villagers and their details:\n";
+    for (auto &pair : villagers) {
+        cout << pair.first << " ["
+             << get<0>(pair.second) << ", "
+             << get<1>(pair.second) << ", "
+             << get<2>(pair.second) << "]\n";
+    }
 
   
     return 0;
